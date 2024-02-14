14364bd5b53720cc038ac06251a5f416e52b5ad0
Shubham Parmar <shubhamparmarmoney@gmail.com>
2024-02-14 17:13:37 +0530

    Completed Anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,32 @@
 */
 
 function isAnagram(str1, str2) {
-
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  let obj = {};
+  for (let i = 0; i < str1.length; i++) {
+    if (obj[str1[i]]) {
+      obj[str1[i]] += 1;
+    } else {
+      obj[str1[i]] = 1;
+    }
+  }
+  for (let i = 0; i < str2.length; i++) {
+    if (obj[str2[i]]) {
+      obj[str2[i]] -= 1;
+    } else {
+      return false;
+    }
+  }
+  for (let key in obj) {
+    if (obj[key] !== 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
