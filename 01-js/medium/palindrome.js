56478d92196ae24d657f1744dbdb86e94969dd49
Shubham Parmar <shubhamparmarmoney@gmail.com>
2024-02-14 17:16:04 +0530

    Completed Palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,6 +4,23 @@
 */
 
 function isPalindrome(str) {
+  str = str.toLowerCase();
+  let punctuations = ['~', '!', '?', ',', ';' , '.'];
+  let i = 0;
+  let j = str.length-1;
+  while(i<=j) {
+    if(str.charAt(i) === ' ' || punctuations.includes(str.charAt(i))){
+      i++;
+      continue;
+    }
+    if(str.charAt(j) === ' ' || punctuations.includes(str.charAt(j))) {
+      j--;
+      continue;
+    }
+    if(str.charAt(i) != str.charAt(j)) return false;
+    i++;
+    j--;
+  }
   return true;
 }
 
