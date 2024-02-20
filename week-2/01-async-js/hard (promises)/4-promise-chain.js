8700cc839f33078eed1b422cb8118b27d792eb4b
Shubham Parmar <shubhamparmarmoney@gmail.com>
2024-02-20 16:21:36 +0530

    Solved assignments of week-2

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -4,21 +4,37 @@
  * Return a promise chain which return the time in milliseconds it takes to complete the entire operation.
  * Compare it with the results from 3-promise-all.js
  */
-
+function delay(t) {
+        return new Promise((resolve)=>{
+                setTimeout(resolve, t*1000);
+        });
+}
 function wait1(t) {
-
+        return delay(t);
 }
 
 function wait2(t) {
-
+        return delay(t);
 }
 
 function wait3(t) {
-
+        return delay(t);
 }
 
 function calculateTime(t1, t2, t3) {
-
+        let d = new Date();
+        let startTime = d.getTime();
+        return new Promise((resolve)=>{
+                return wait1(t1).then(()=>{
+                        return wait2(t2).then(()=>{
+                                return wait3(t3).then(()=>{
+                                        d = new Date();
+                                        let endTime = d.getTime();
+                                        resolve(endTime-startTime);
+                                });
+                        });
+                });
+        });
 }
 
 module.exports = calculateTime;
